a1fc374c03cfc007d50223df427cfeb21b0f0e65
gabrielwallacess <gsantos2018@gmail.com>
2026-09-19 15:45:31 -0300

    ok

--- scripts/data.js
+++
@@ -1,5 +0,0 @@
-const currentYear = document.querySelector("#anoAtual");
-const lastModified = document.querySelector("#ultimaModificacao");
-
-currentYear.textContent = new Date().getFullYear();
-lastModified.textContent = `Última Modificação: ${document.lastModified}`;
